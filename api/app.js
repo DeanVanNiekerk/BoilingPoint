b34121a7dae7b74bc73e6bbc7243d4d50dd319bd
Marius Potgieter <aelrathza@gmail.com>
2015-11-20 11:08:27 +0200

    NodeJS Port Settings
    Added nodejs default port settings to app.js to bind to port 8080

--- api/app.js
+++ api/app.js
@@ -60,6 +60,8 @@ app.use(function(err, req, res, next) {
 });
 
 
-
+// sets port 8080 to default or unless otherwise specified in the environment
+app.set('port', process.env.PORT || 8080);
+app.listen(app.get('port'));
 
 module.exports = app;
