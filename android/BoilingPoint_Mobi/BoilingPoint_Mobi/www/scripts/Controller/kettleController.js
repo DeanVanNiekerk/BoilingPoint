93d98d211098f8f4412c58823770aa19865a353f
TripWireZa <tripwire.rsa@gmail.com>
2016-01-15 13:45:03 +0200

    Added getKettleStatus

--- android/BoilingPoint_Mobi/BoilingPoint_Mobi/www/scripts/Controller/kettleController.js
+++ android/BoilingPoint_Mobi/BoilingPoint_Mobi/www/scripts/Controller/kettleController.js
@@ -1,38 +1,44 @@
-﻿kettleApp.controller("kettleController", function ($scope, kettleservice) {
+﻿
+
+kettleApp.controller("kettleController", function ($scope, kettleservice) {
 
     $scope.status = new StatusModel(null);
     $scope.errorResult = '';
 
     var service = kettleservice;
-
+    //var service = fakekettleservice
     
     //Fetch current status from kettle service
     $scope.getKettleStatus = function () {
         
-        var data = JSON.parse('{ "Type": "2", "Data": { "On": "true", "Temp": "35", "Level": "50%" } }');
+        //var data = JSON.parse('{ "Type": "2", "Data": { "On": "true", "Temp": "35", "Level": "50%" } }');
 
-        $scope.status = new StatusModel(data);
-        $scope.errorResult = '';
+        //$scope.status = new StatusModel(data);
+        //$scope.errorResult = '';
+        service.getKettleStatus().then(
+            //Success callback
+            function (response) {
+                debugger;
+                var json = JSON.parse(response.data);
+                $scope.status = new StatusModel(json);
+                $scope.errorResult = '';
+                debugger;
+                //debug
+                var jsonPretty = JSON.stringify(json, null, '\t');
+                $("pre").text(jsonPretty);
+            },
+            //Fail callback
+            function (response) {
+                debugger;
+                if (response.status == 503) {
+                    $scope.status = new StatusModel(null);
+                    $scope.errorResult = "No kettle connected.";
+                    return;
+                }
 
-        //service.getKettleStatus().then(
-        //    //Success callback
-        //    function (response) {
-        //        var json = JSON.parse(response.data);
-        //        $scope.status = new StatusModel(json);
-        //        $scope.errorResult = '';
-        //    },
-        //    //Fail callback
-        //    function (response) {
-
-        //        if (response.status == 503) {
-        //            $scope.status = new StatusModel(null);
-        //            $scope.errorResult = "No kettle connected.";
-        //            return;
-        //        }
-
-        //        $scope.errorResult = "Error Getting Status. "
-        //        $scope.errorResult += "Status Code: " + response.status;
-        //    });
+                $scope.errorResult = "Error Getting Status. "
+                $scope.errorResult += "Status Code: " + response.status;
+            });
     };
 
     $scope.toggleOnOff = function () {
@@ -47,9 +53,11 @@
 
     $scope.turnKettleOn = function () {
         $scope.errorResult = 'on clicked';
+        //service.turnKettleOn()
     };
     $scope.turnKettleOff = function () {
         $scope.errorResult = 'off clicked';
+        //service.turnKettleOff
     };
 
     setInterval($scope.getKettleStatus(), 5000); // Time in milliseconds
